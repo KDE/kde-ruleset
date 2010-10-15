1a24eeaf6efc5bb08786f047f0edd1da59a23864
Torgny Nyblom <kde@nyblom.org>
2010-10-15 07:35:48 +0200

    Misc fixes and improvements

--- bin/trackModule.pl
+++ bin/trackModule.pl
@@ -4,24 +4,35 @@
 use strict;
 use warnings;
 use Switch;
+use File::Basename;
+use Data::Dumper;
 
 $| = 1;
 
+my %gargs = (
+    'repository' => "",
+    'path' => "",
+    'revision' => "HEAD",
+    'subdirs' => 0,
+    'showcommands' => 0,
+    'ignore' => "",
+    'module' => "",
+    'recurseDepth' => 0
+);
 my @commands = ();
 my $firstrev = 9999999;
-my $module = "";
-
+my %parentMoves;
 sub listSubDirs
 {
-    print("Getting a list of subdirs...\n");
+    #print("Getting a list of subdirs...\n");
     my ($repository, $revision, $root, $path, $recurse, $ignore) = @_;
     my $cmd = "svn ls $repository/$root/$path\@$revision";
     push( @commands, $cmd );
 
     my @dirs;
-    open( my $CMD, "$cmd |" ) || die "listSubDirs: Failed to run \"$cmd\": $!\n";
+    open( my $CMD, "-|", "$cmd" ) || die "listSubDirs: Failed to run \"$cmd\": $!\n";
 
-    print( "listSubDirs: Getting subdirs of '$repository / $root / $path \@ $revision'\n" );
+    #print( "listSubDirs: Getting subdirs of '$repository / $root / $path \@ $revision'\n" );
     while( <$CMD> ) {
         chomp;
         if( /(\S+)\/$/ ) {
@@ -31,9 +42,9 @@ sub listSubDirs
             if( length( $path ) > 0 && substr( $path, -1 ) ne "/" ) {
                 $path = "$path/";
             }
-            print( "listSubDirs:\tAdding: '$path$_'\n" );
+            print( "listSubDirs:\tAdding: $path$_\n" );
             push( @dirs, "$path$1" );
-            push( @dirs, listSubDirs($repository, $revision, $root, "$path$1", $recurse) ) if( $recurse )
+            push( @dirs, listSubDirs($repository, $revision, $root, "$path$1", ++$recurse, $ignore) ) if( $recurse < $gargs{'recurseDepth'})
         }
     }
     close( $CMD );
@@ -42,18 +53,19 @@ sub listSubDirs
 
 sub getCopyFrom
 {
-    my ($repository, $revision, $path ) = @_;
-    my $cmd = "svn log -v --stop-on-copy $repository/$path\@$revision";
-    print( "getCopyFrom: Getting origin of '$repository / $path \@ $revision...\n" );
+    my $args = $_[0];
+    my %returns = ('type' => '', 'path' => '','fromRev' => 0, 'minRev' => 0 );
+    my $cmd = "svn log -v --stop-on-copy $args->{'repository'}/$args->{'path'}\@$args->{'rev'}";
+    #print( "getCopyFrom: Getting origin of '$repository / $path \@ $revision...\n" );
     push( @commands, $cmd );
-    $path = "/$path";
+    my $path = "/$args->{'path'}";
     my $fromRevision = 0;
     my $fromPath = "";
 
     my @log = ();
 
-    open( CMD, "$cmd |" ) || die "getCopyFrom: Failed to run \"$cmd\": $!\n";
-    while( <CMD> ) {
+    open( my $CMD, "-|", "$cmd" ) || die "getCopyFrom: Failed to run \"$cmd\": $!\n";
+    while( <$CMD> ) {
         chomp;
         if( /^r(\d+) \| / ) {
             @log = ();
@@ -65,8 +77,11 @@ sub getCopyFrom
     my $cvs2svnCandidate = 0;
     my $missedCopy = 0;
     my $currentRev = 0;
+    my $delayedCopy = 0;
+    my $delayedCopyCandidate = 0;
+    my $verbose = 0;
 
-    open( FILE, ">>$module-rules-auto" );
+    open( my $FILE, ">>", "$gargs{'module'}-rules-auto" ) || die "!$!\n";
     foreach( @log ) {
         if( /^r(\d+) \| / ) {
             $currentRev = $1;
@@ -82,21 +97,25 @@ sub getCopyFrom
             my @components = split( /\//, "$path" );
             my $subdir = pop( @components );
             my $shortPath = join( "/", @components );
-            #print("> ------\n");
-            #print("> \$_: $_\n");
-            #print("> \$path:         $path\n");
-            #print("> \$newPath:      $newPath\n");
-            #print("> \$origPath:     $origPath\n");
-            #print("> \$fromRevision: $fromRevision\n");
-            #print("> \$subdir:       $subdir\n");
-            #print("> \$components:   @components\n");
-            #print("> \$shortPath:    $shortPath\n");
-
+            if( $verbose ) {
+                print("> ------\n");
+                print("> \$_: $_\n");
+                print("> \$path:         $path\n");
+                print("> \$newPath:      $newPath\n");
+                print("> \$origPath:     $origPath\n");
+                print("> \$fromRevision: $fromRevision\n");
+                print("> \$subdir:       $subdir\n");
+                print("> \$components:   @components\n");
+                print("> \$shortPath:    $shortPath\n");
+                print("> \$delayedCopyCandidate:    $delayedCopyCandidate\n");
+            }
             # case 1: the dir is moved/replaced
             if( $newPath =~ /$path$/ ) {
                 print( "getCopyFrom:\t\@r$currentRev: Found '$newPath' from '$origPath' at rev $fromRev\n" );
-                print( FILE "#\t[ Move/Replace: $newPath @ $currentRev -> $origPath @ $fromRev ]\n" );
-                $fromPath = $origPath;
+                print( $FILE "#\t[ Move/Replace: $newPath @ $currentRev <- $origPath @ $fromRev ]\n" );
+                $returns{'path'} = $origPath;
+                $returns{'fromRev'} = $fromRev;
+                $returns{'type'} = "move";
                 last;
             }
 
@@ -108,36 +127,56 @@ sub getCopyFrom
                 if( defined $1 ) {
                     $tmp = $1;
                 }
-                $fromPath = "$origPath$tmp/$subdir";
-                print( FILE "#\t[ Parent: $newPath @ $currentRev -> $origPath @ $fromRev ]\n" );
+                $returns{'path'} = "$origPath$tmp/$subdir";
+                $returns{'fromRev'} = $fromRev;
+                $returns{'type'} = "parent";
+                my %history = %returns;
+                $history{'path'} = $newPath;
+                if( exists $parentMoves{"$newPath:$fromRev"} ) {
+                    $returns{'type'} = "skip";
+                    #print "> Skipping\n";
+                } else {
+                    $parentMoves{"$newPath:$fromRev"} = 1;
+                }
+                #print "Parents: " . Dumper(%parentMoves);
+                print( $FILE "#\t[ Parent: $newPath @ $currentRev <- $origPath @ $fromRev ]\n" );
                 last;
+            }
+
+            # case 3:
+            if( $delayedCopyCandidate == 1 && $newPath =~ /$path\// ) {
+                print(" getCopyFrom:\t\@r$currentRev: Found dir add with delayed file move/copy '$newPath' from '$origPath' at rev $fromRev\n" );
+                my($file, $dir, $suffix) = fileparse($origPath);
+                $origPath = substr($dir, 0, -1);
+                print( $FILE "#\t[ Delayed: $newPath @ $currentRev <- $origPath @ $fromRev ]\n" );
+                $delayedCopy = 1;
+                #last;
             }
         }
 
-        # case 3: A cvs2svn server side move
-        # Directory add followed by every file as a svn cp
-        if( /\s+A\s+$path/ ) {
+        # case 3: The directory is added and then populated with files from another path
+        # case 4: A cvs2svn server side move, Directory add followed by every file as a svn cp
+        if( /\s+A\s+$path$/ ) {
+            $delayedCopyCandidate = 1;
             $cvs2svnCandidate = 1;
+            print "> cvs2svn or delayed copy ($_)...\n";
         }
+
         if( $cvs2svnCandidate && /cvs2svn/ ) {
             $cvs2svnUsed = 1;
+            $missedCopy = 0;
             last;
-        } elsif( /\s+A (\S*$path\S+) \(from (\/\S+):(\d+)\)/ ) {
-            $missedCopy = 1;
+        } elsif( $delayedCopyCandidate == 1 && /\s+A (\/$path\/\S+) \(from (\/\S+):(\d+)\)/ ) {
+            $delayedCopy = 1;
         }
     }
 
-    if( $missedCopy ) {
-        my $msg = "WARNING: $path was added in r$currentRev and files/dirs was copied into it but no 'from' path was detected, history loss possible.\n";
-        print( FILE "# $msg" );
-        print( $msg );
-    }
     if( $cvs2svnUsed ) {
         foreach( @log ) {
             if( /^r(\d+) \| / ) {
                 $currentRev = $1;
             }
-            if( /\s+A\s+$path\S+\s+\(from (\S+):(\d+)\)/ ) {
+            if( /\s+A\s+\/$path\/\S+\s+\(from (\S+):(\d+)\)/ ) {
                 my $origPath = $1;
                 my $fromRev = $2;
                 my @components = split( /\//, $path );
@@ -153,128 +192,127 @@ sub getCopyFrom
                 if( $origPath eq "/" ) {
                     print( "\n==========================\n" );
                     print( "Unable to determine the source of the move, aborting this operation.\n" );
-                    print( "To manually track this start with \"svn log -v -c$currentRev $repository\"\n" );
-                    print( "You may then restart this tool with the new parameters or you can do it manually.\n" );
+                    print( "To manually track this start with \"svn log -v -c$currentRev $gargs{'repository'}\"\n" );
                     print( "Sorry for the extra trouble!\n" );
                     print( "==========================\n" );
-                    my @returnValue = (0,"");
-                    return @returnValue;
+                    return \%returns;
                 }
 
                 print( "getCopyFrom:\t\@r$currentRev: Found cvs2svn move '$path' from '$origPath' at rev $fromRev\n" );
-                print( FILE "#\t[ cvs2svn: $path @ $currentRev -> $origPath @ $fromRev ]\n" );
-                $fromPath = $origPath;
-                $fromRevision = $fromRev;
+                print( $FILE "#\t[ cvs2svn: $path @ $currentRev -> $origPath @ $fromRev ]\n" );
+                $returns{'path'} = $origPath;
+                $returns{'fromRev'} = $fromRev;
+                $returns{'type'} = "cvs2svn";
+                $missedCopy = 0;
                 last;
             }
         }
     }
-    close( FILE );
-    close( CMD );
-    if( $fromPath eq "" ) {
-        $fromRevision = 0;
-    } elsif( substr($fromPath,0,1) eq "/" ) {
-        $fromPath = substr($fromPath, 1);
+
+    if( $missedCopy == 1 ) {
+        my $msg = "#\tWARNING:\n#\t$path was added in r$currentRev and file(s)/dir(s) was copied into it but no 'from' path was detected, history loss possible.\n";
+        print( $FILE "#\t$msg" );
+        print( $msg );
     }
-    my @returnVale = ( $fromRevision, $fromPath );
-    return @returnVale;
+    if( $delayedCopy == 1 ) {
+        print( "WARNING:\t$path was added in r$currentRev and file(s)/dir(s) was copied into it, a 'from' path was detected but it might contain extra file(s)/dir(s).\n" );
+    }
+
+    close( $FILE );
+    close( $CMD );
+    if( substr($returns{'path'},0,1) eq "/" ) {
+        $returns{'path'} = substr($returns{'path'}, 1);
+    }
+    return \%returns
+
 }
 
 sub getCopyFromRecursive
 {
-    my ($repository, $module, $revision, $root, $path ) = @_;
-    print("\n------------------------------\n");
-    print("$repository, $module, $revision, $root, $path\n" );
+    my $args = $_[0];
+    #my ($repository, $module, $revision, $root, $path ) = @_;
+    #print("\n------------------------------\n");
+    #print("$repository, $module, $revision, $root, $path\n" );
     my @history = ();
-    my $fromRev = $revision;
-    my $fromPath = "$root";
-    $fromPath = "$root/$path" if( $path ne "" );
-
-    push( @history, [$path, $revision] );
+    my $fromRev = $args->{'rev'};
+    my $fromPath = $args->{'root'};
+    $fromPath = "$args->{'root'}/$args->{'path'}" if( $args->{'path'} ne "" );
+    
+    open(my $FILE, ">>", "$gargs{'module'}-rules-auto") || die "!!$!\n";
+    print( $FILE "#\t------< $fromPath >------\n" );    
+    my $ret = {};
+    push( @history, { 'type' => 'add', 'path' => $args->{'path'}, 'fromRev' => $args->{'rev'}, 'minRev' => 0 } );
     do {
-        ($fromRev, $fromPath) = getCopyFrom( $repository, $fromRev, $fromPath );
-        if( $fromRev > 1) {
-            push( @history, [$fromPath, $fromRev] );
+        my %vars = ( 'repository' => $args->{'repository'}, 'rev' => $fromRev, 'path' => $fromPath );
+        $ret = getCopyFrom( \%vars );
+        $fromRev = $ret->{'fromRev'};
+        $fromPath = $ret->{'path'};
+        if( $ret->{'fromRev'} > 1 && $ret->{'type'} ne "skip" ) {
+            push( @history, $ret );
         }
 
-    } while( $fromRev > 0 );
+    } while( $ret->{'fromRev'} > 0 );
 
-    open(FILE, ">>$module-rules-auto");
     my $minRevision = 0;
     for my $historyPart ( reverse( @history ) ) {
-        push( @$historyPart, $minRevision );
-        $minRevision = @$historyPart[1];
+        $historyPart->{'minRev'} = $minRevision;
+        $minRevision = $historyPart->{'fromRev'};
     }
 
     for my $historyPart (@history) {
-        if( @$historyPart[1] ne "HEAD" || @$historyPart[2] ne "0" ) {
-           print(FILE "#\t[ @$historyPart ]\n");
+        if( $historyPart->{'fromRev'} ne "HEAD" || $historyPart->{'minRev'} ne "0" ) {
+           print($FILE "#\t[ $historyPart->{'type'} $historyPart->{'path'} $historyPart->{'fromRev'} $historyPart->{'minRev'} ]\n");
 
-            if( @$historyPart[0] eq "" ) {
-                print( FILE "match /$root/\n");
-            } elsif( @$historyPart[0] eq "$path" ) {
-                print( FILE "match /$root/$path/\n" );
+            if( $historyPart->{'path'} eq "" ) {
+                print( $FILE "match /$args->{'root'}/\n");
+            } elsif( $historyPart->{'path'} eq "$args->{'path'}" ) {
+                print( $FILE "match /$args->{'root'}/$args->{'path'}/\n" );
             } else {
-                print(FILE  "match /@$historyPart[0]/\n");
+                print( $FILE  "match /$historyPart->{'path'}/\n");
             }
-            print(FILE  "    repository $module\n");
-            if( $root ne @$historyPart[0] ) {
-                my $prefix = "$path";
-                print(FILE  "    prefix $prefix/\n" ) if( $prefix ne "" );
+            print( $FILE  "    repository $args->{'module'}\n");
+            if( $args->{'root'} ne $historyPart->{'path'} ) {
+                my $prefix = $args->{'path'};
+                print( $FILE  "    prefix $prefix/\n" ) if( $prefix ne "" );
             }
-            print(FILE  "    branch master\n");
-            print(FILE  "    max revision @$historyPart[1]\n") if( @$historyPart[1] ne "HEAD" );
-            print(FILE  "    min revision @$historyPart[2]\n" ) if( @$historyPart[2] != 0 );
-            print(FILE  "end match\n\n");
+            print( $FILE  "    branch master\n");
+            print( $FILE  "    max revision $historyPart->{'fromRev'}\n") if( $historyPart->{'fromRev'} ne "HEAD" );
+            print( $FILE  "    min revision $historyPart->{'minRev'}\n" ) if( $historyPart->{'minRev'} != 0 );
+            print( $FILE  "end match\n\n");
         } else {
-            print( FILE "#\t[ @$historyPart ]\n" );
+            print( $FILE "#\t[ $historyPart->{'type'} $historyPart->{'path'} $historyPart->{'fromRev'} $historyPart->{'minRev'} ]\n");
         }
     }
-    close(FILE);
-    print("\n------------------------------\n");
+    close($FILE);
+    #print("\n------------------------------\n");
 }
 
-my $repository;
-my $path;
-my $revision = "HEAD";
 my $argnum;
-my $subdirs = 0;
-my $showcommands = 0;
-my $recurse = 0;
-my $ignore = "";
 
 for($argnum = 0; $argnum <= $#ARGV; $argnum++ ) {
     print "Argument #$argnum: $ARGV[$argnum]\n";
     switch ($ARGV[$argnum]) {
-        case "--repo"   { $repository = $ARGV[++$argnum]; }
-        case "--path"   { $path = $ARGV[++$argnum]; }
-        case "--module" { $module = $ARGV[++$argnum]; }
-        case "--rev"    { $revision = $ARGV[++$argnum]; }
-        case "--subdirs"{ $subdirs = 1; }
-        case "--showcommands" { $showcommands = 1; }
-        case "--recurse" { $recurse = 1; }
-        case "--ignore" { $ignore = $ARGV[++$argnum]; }
+        case "--repo"   { $gargs{'repository'} = $ARGV[++$argnum]; }
+        case "--path"   { $gargs{'path'} = $ARGV[++$argnum]; }
+        case "--module" { $gargs{'module'} = $ARGV[++$argnum]; }
+        case "--rev"    { $gargs{'revision'} = $ARGV[++$argnum]; }
+        case "--subdirs"{ $gargs{'subdirs'} = 1; }
+        case "--showcommands" { $gargs{'showcommands'} = 1; }
+        case "--recurse" { $gargs{'recurseDepth'} = $ARGV[++$argnum]; }
+        case "--ignore" { $gargs{'ignore'} = $ARGV[++$argnum]; }
         else            { print "Unknown option: $ARGV[$argnum]\n"; exit; }
     }
 }
-if( $#ARGV < 5 || $#ARGV > 11 ) {
-    print( "Usage: trackModule.pl --repo repository --path path --module module [--rev revision] [--subdirs] [--showcommands] [--recurse] [--ignore path]\n");
+if( $#ARGV < 5 || $#ARGV > 12 ) {
+    print( "Usage: trackModule.pl --repo repository --path path --module module [--rev revision] [--subdirs] [--showcommands] [--recurse depth] [--ignore path]\n");
     exit;
 }
-print <<EOF;
-Got:
-  \$repository: $repository
-  \$path:       $path
-  \$module:     $module
-  \$revision:   $revision
-  \$subdirs:    $subdirs
-  \$recurse:    $recurse
-  \$ignore:     $ignore
-EOF
+print Dumper(%gargs);
 
 my @dirs = ("");
-if( $subdirs ) {
-    my @subdirs = listSubDirs( $repository, $revision, $path, "", $recurse, $ignore );
+if( $gargs{'subdirs'} != 0 ) {
+    print("Getting a list of subdirs...\n");
+    my @subdirs = listSubDirs( $gargs{'repository'}, $gargs{'revision'}, $gargs{'path'}, "", 0, $gargs{'ignore'} );
     push( @dirs, @subdirs );
 }
 
@@ -282,28 +320,29 @@ if( $subdirs ) {
 
 my %foundDirRevisions;
 
-open(FILE, ">$module-rules-auto") || die;
-print(FILE "create repository $module\n");
-print(FILE "end repository\n\n");
-close(FILE);
+open(my $FILE, ">", "$gargs{'module'}-rules-auto") || die "!$!\n";
+print( $FILE "create repository $gargs{'module'}\n");
+print( $FILE "end repository\n\n");
+close( $FILE);
 foreach( @dirs )
 {
-    print( "Scanning '$_'...\n" );
-    getCopyFromRecursive( $repository, $module, $revision, $path, $_ );
+    print( "Scanning '$gargs{'path'} / $_'...\n" );
+    my %arg = ( 'repository' => $gargs{'repository'}, 'module' => $gargs{'module'}, 'rev' => $gargs{'revision'}, 'root' => $gargs{'path'}, 'path' => $_ );
+    getCopyFromRecursive( \%arg );
 }
 
-if( $showcommands ) {
-    open(FILE, ">>$module-rules-auto");
-    print(FILE "#Used commands...\n" );
+if( $gargs{'showcommands'} != 0 ) {
+    open( $FILE, ">>", "$gargs{'module'}-rules-auto") || die "!$!\n";
+    print( $FILE "#Used commands...\n" );
     foreach( @commands ) {
-        print(FILE "#$_\n" );
+        print( $FILE "#$_\n" );
     }
-    close(FILE);
+    close( $FILE);
 }
 
-print( "First revision used: $firstrev" );
-open(FILE, ">>$module-rules-auto") || die;
-print(FILE "match /\n");
-print(FILE "end match\n");
-print(FILE "#First revision used: $firstrev\n" );
-close(FILE);
+print( "First revision used: $firstrev\n" );
+open( $FILE, ">>", "$gargs{'module'}-rules-auto") || die "!$!\n";
+print($FILE "match /\n");
+print($FILE "end match\n");
+print($FILE "#First revision used: $firstrev\n" );
+close($FILE);
