b0320a461924c0646ccfda427bddc2892931bf33
Parker Coates <coates@kde.org>
2012-03-11 18:49:47 -0400

    Add recompress-svgz-and-png.sh and nullgzip.py tools.
    nullgzip.py converts a gzip file to a null-compressed gzip file having
    a valid gzip header, but completely uncompressed contents. This is
    useful to produce valid SVGZ files that git can see as plain text and
    produce meaningful deltas for.
    
    recompress-svgz-and-png.sh rewrites the history of a repository,
    converting all SVGZ files to null compression and applying aggressive
    compression to all PNG files, so that "optimize-graphics" commits don't
    lead to multiple copies of the same file in the history only differing
    by their compression.

---
+++ bin/nullgzip.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python
+
+from __future__ import print_function
+import gzip
+import sys
+
+CHUNKSIZE = 4096
+
+if len(sys.argv) != 3:
+	print('Usage: %s <input.gz> <output.gz>' % sys.argv[0], file=sys.stderr)
+	print('Creates an uncompressed gzip file from a gzip input file.', file=sys.stderr)
+	sys.exit(0)
+
+try:
+	inFile = gzip.open(sys.argv[1], 'rb')
+except IOError:
+	print('Cannot open input file.', file=sys.stderr)
+	sys.exit(1)
+	
+try:
+	outFile = gzip.open(sys.argv[2], 'wb', 0)
+except IOError:
+	print('Cannot open output file.', file=sys.stderr)
+	sys.exit(1)
+
+try:
+	with inFile, outFile:
+		bytes = inFile.read(CHUNKSIZE)
+		while bytes:
+			outFile.write(bytes)
+			bytes = inFile.read(CHUNKSIZE)
+except IOError:
+	print('Error while copying content from input file to output file.', file=sys.stderr)
+	sys.exit(1)
+
---
+++ bin/recompress-svgz-and-png.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+command -v optipng >/dev/null || { echo "optipng not installed. Aborting."; exit 1; }
+command -v advdef >/dev/null || { echo "advdef not installed. Aborting."; exit 1; }
+
+bindir="${RULESETDIR:?\$RULESETDIR must point to the kde-ruleset directory}/bin"
+source "$bindir/filter-goodies"
+
+tempdir=$(calc_tempdir recompress-images)
+cachedir=$(calc_tempdir image-cache)
+
+rm -rf "$tempdir";
+mkdir -p "$cachedir";
+
+export cachedir
+export bindir
+
+git filter-branch -f -d "$tempdir" --tag-name-filter cat --tree-filter "$(cat <<'EOF'
+
+git ls-tree -r "$GIT_COMMIT" | egrep '\.(svgz|png)$' | while read objtype objmode objhash objname; do
+    if [ ! -f "$cachedir/$objhash" ]; then
+        if [ -z "$nl" ]; then echo >&2; nl=y; fi
+        echo "${objname}:$(echo "$objhash" | cut -c1-10) not in cache, recompressing" >&2
+        case $objname in
+            *.svgz)
+                $bindir/nullgzip.py "$objname" "$cachedir/$objhash"
+                ;;
+            *.png)
+                optipng -o5 "$objname" &&
+                advdef -z4 "$objname" &&
+                cp "$objname" "$cachedir/$objhash"
+                ;;
+        esac
+    fi
+    cp -f "$cachedir/$objhash" "$objname"
+done
+EOF
+)" -- --all
+
+
