2216ce7e35d571a31836b60371f620a844ece91d
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2012-03-13 08:13:29 +0100

    libkmahjongg-postprocess.sh did too much

--- kdegames/libkmahjongg-postprocess.sh
+++ kdegames/libkmahjongg-postprocess.sh
@@ -9,45 +9,28 @@ then
 fi
 
 export RULESETDIR=`pwd`/kde-ruleset
+export REPO=libkmahjongg
 
-# if we have a copy of what svn2git created, start over with that copy
-
-if test -d libkmahjongg.org
+if test ! -d $REPO
 then
-	rm -rf libkmahjongg
-	cp -a libkmahjongg.org libkmahjongg
+	echo repository $REPO not found
+	exit 1
 fi
 
-cd libkmahjongg
+# if we have a copy of what svn2git created, start over with that copy
+if test -d $REPO.org
+then
+	rm -rf $REPO
+	cp -a $REPO.org $REPO
+fi
+
+cd $REPO
 
 bindir="${RULESETDIR:?\$RULESETDIR must point to the kde-ruleset directory}/bin"
 source "$bindir/filter-goodies"
 
-echo 'filter-branch...'
-tempdir="$(calc_tempdir libkmahjongg-filter)"
-
-# r634260 contains only the changed files in that branch
-# This makes the git revision appear as deleting all the rest of the tree.
-# Here we filter all revisions in the branch (both of them!)
-# to contain the rest of the files, taken from the master revision right before branching.
-
-# this code initially copied from kdegraphics/okular, but did not work for me.
-# I had to replace master.. by ${prevmaster}..
-
-prevmaster=$(echo 629421 | $bindir/translateRevlist.py)
-
-git filter-branch -f -d "$tempdir" \
-    --tree-filter "git read-tree $prevmaster" \
-    ${prevmaster}..workbranch/kconfiggroup_port
-
-echo 'delete_fb_backups...'
-delete_fb_backups
-
 echo 'parent-adder...'
-$bindir/parent-adder $RULESETDIR/kdegames/libkmahjongg-parentmap
+$bindir/parent-adder $RULESETDIR/kdegames/$REPO-parentmap
 
 echo 'delete_fb_backups...'
 delete_fb_backups
-
-echo 'fix-tags...'
-$bindir/fix-tags
