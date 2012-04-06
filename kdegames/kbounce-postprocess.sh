9abe57a91ff78f3e3468c2439f893dae91d27095
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2012-04-05 22:58:33 -0300

    Remove all the work-branch ignoring from kbounce.
    This made me found a branch was being removed when it shouldn't have.
    Added parentmap for it.

--- kdegames/kbounce-postprocess.sh
+++ kdegames/kbounce-postprocess.sh
@@ -7,26 +7,6 @@ set -e
 bindir="${RULESETDIR:?\$RULESETDIR must point to the kde-ruleset directory}/bin"
 source "$bindir/filter-goodies"
 
-git update-ref -d refs/backups/r467490/tags/v3.4.3
-git update-ref -d refs/backups/r519761/tags/v3.5.2
-git update-ref -d refs/backups/r591395/tags/v3.5.5
-
-# why are those here at all? rules should have excluded them
-git update-ref -d refs/workbranch/qdbus-api-changes
-git update-ref -d refs/workbranch/kparts_urlargs_split
-git update-ref -d refs/workbranch/kconfig_new
-git update-ref -d refs/workbranch/kconfiggroup_port
-git update-ref -d refs/workbranch/kde4_jobflags
-git update-ref -d refs/workbranch/kde4_kconfig
-git update-ref -d refs/workbranch/kaction-cleanup1
-git update-ref -d refs/workbranch/kaction-cleanup2
-git update-ref -d refs/workbranch/kaction-cleanup3
-git update-ref -d refs/workbranch/systray-rewrite
-git update-ref -d refs/workbranch/systray-rewrite-tng
-
-# unannotated tag cannot be pushed to KDE
-#git tag -d backups/master@473660
-
 echo 'parent-adder...'
 $bindir/parent-adder $RULESETDIR/kdegames/kbounce-parentmap
 
@@ -36,7 +16,7 @@ delete_fb_backups
 echo 'fix-tags...'
 $bindir/fix-tags
 delete_fb_backups
- 
+
 change_cvs2svn_author
 delete_fb_backups
 
