c9fa304f70ab48819b75ee9acbe8d9ba715d5f2e
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2012-04-07 01:34:58 -0300

    kolf: Get all work branches.
    Remove all the ignores and deletions for global work branches
    and add missing parentmap entries for the branches that were
    previously ignored.

--- kdegames/kolf-postprocess.sh
+++ kdegames/kolf-postprocess.sh
@@ -7,24 +7,6 @@ set -e
 bindir="${RULESETDIR:?\$RULESETDIR must point to the kde-ruleset directory}/bin"
 source "$bindir/filter-goodies"
 
-git update-ref -d refs/backups/r467490/tags/v3.4.3
-git update-ref -d refs/backups/r519761/tags/v3.5.2
-git update-ref -d refs/backups/r591395/tags/v3.5.5
-
-# why are those here at all? rules should have excluded them
-git update-ref -d refs/workbranch/qdbus-api-changes
-git update-ref -d refs/workbranch/kparts_urlargs_split
-git update-ref -d refs/workbranch/kconfig_new
-git update-ref -d refs/workbranch/kconfiggroup_port
-git update-ref -d refs/workbranch/kde4_jobflags
-git update-ref -d refs/workbranch/kde4_kconfig
-git update-ref -d refs/workbranch/kaction-cleanup1
-git update-ref -d refs/workbranch/kaction-cleanup2
-git update-ref -d refs/workbranch/kaction-cleanup3
-git update-ref -d refs/workbranch/systray-rewrite
-git update-ref -d refs/workbranch/systray-rewrite-tng
-
-
 echo 'parent-adder...'
 $bindir/parent-adder $RULESETDIR/kdegames/kolf-parentmap
 
