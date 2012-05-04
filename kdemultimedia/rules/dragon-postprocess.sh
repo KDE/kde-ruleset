55f14173c652feb80ae84a2d1db44fe9ad9ba30f
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2012-05-04 20:07:22 -0300

    Change name of the temporary remote to "mpris".

--- kdemultimedia/rules/dragon-postprocess.sh
+++ kdemultimedia/rules/dragon-postprocess.sh
@@ -9,15 +9,15 @@ last_mpris=53b124b07da4432a741425ae1a4081b707024736
 
 [[ $(git rev-parse master) == $last_master ]]
 
-git remote add hein git://anongit.kde.org/scratch/hein/dragon.git
-trap "git remote rm hein" 0
-git fetch hein
+git remote add mpris git://anongit.kde.org/scratch/hein/dragon.git
+trap "git remote rm mpris" 0
+git fetch mpris
 
-[[ $(git rev-parse hein/master) == $last_mpris ]]
-git rev-list master..hein/master | grep -q "$first_mpris"
+[[ $(git rev-parse mpris/master) == $last_mpris ]]
+git rev-list master..mpris/master | grep -q "$first_mpris"
 
 echo "$first_mpris" "$last_master" > "$(git rev-parse --git-dir)/info/grafts"
-git filter-branch master..hein/master
-git update-ref refs/heads/master $(git rev-parse hein/master) $last_master
+git filter-branch master..mpris/master
+git update-ref refs/heads/master $(git rev-parse mpris/master) $last_master
 
 git for-each-ref --format="%(refname)" ${1:-refs/original/} | xargs --no-run-if-empty -n 1 git update-ref -d
