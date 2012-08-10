2ea1a235e3eb6703d60e4163ba6bebb1be8f4812
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2012-08-10 14:05:18 +0200

    kshisen: merge branches/kde4/

--- kdegames/kshisen-postprocess.sh
+++ kdegames/kshisen-postprocess.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# why are those here at all? rules should have excluded them
 git update-ref -d refs/workbranch/kconfiggroup_port
 git update-ref -d refs/workbranch/kde4_jobflags
 git update-ref -d refs/workbranch/kaction-cleanup2
