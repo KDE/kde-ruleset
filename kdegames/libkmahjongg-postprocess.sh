7c8fdc566dbe83e59b80ad63b9a9439dc5f66cdd
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2012-08-10 10:17:40 +0200

    libkmahjongg: use common-kdegames rules, postprocess

---
+++ kdegames/libkmahjongg-postprocess.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git update-ref -d refs/workbranch/kaction-cleanup2
+git update-ref -d refs/workbranch/kaction-cleanup3
+git update-ref -d refs/workbranch/kinstance-redesign
+git update-ref -d refs/workbranch/kmainwindow-decoupling
+git update-ref -d refs/workbranch/kde4_jobflags
