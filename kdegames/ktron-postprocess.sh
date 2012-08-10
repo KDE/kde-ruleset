dd9e4e6a59d092c78d0d03548adf846f91d54142
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2012-08-10 12:40:53 +0200

    ktron: fixes for postprocessing

--- kdegames/ktron-postprocess.sh
+++ kdegames/ktron-postprocess.sh
@@ -2,14 +2,8 @@
 
 # Execute this script from the converted repository.
 
-git update-ref -d refs/workbranch/kinstance-redesign
 git update-ref -d refs/workbranch/kconfiggroup_port
 git update-ref -d refs/workbranch/kaction-cleanup2
-git update-ref -d refs/workbranch/kmainwindow-decoupling
 
 # TODO r822136 does not move doc back from unmaintained
 # all of doc will only reappear at r924952
-
-rm -rf ../ksnakeduel.prev
-test -d ../ksnakeduel && mv ../ksnakeduel ../ksnakeduel.prev
-mv ../ktron ../ksnakeduel
