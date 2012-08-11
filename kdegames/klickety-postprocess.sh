78c2cb133bdffd1e56694bfd18361e488f3e40f7
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2012-08-11 19:03:58 +0200

    klickety: use common-kdegames rules

---
+++ kdegames/klickety-postprocess.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Execute this script from the converted repository.
+
+git update-ref -d refs/workbranch/kmainwindow-decoupling
+git update-ref -d refs/workbranch/kconfiggroup_port
+git update-ref -d refs/workbranch/kinstance-redesign
+git update-ref -d refs/workbranch/kaction-cleanup1
+git update-ref -d refs/workbranch/kaction-cleanup2
+git update-ref -d refs/workbranch/kaction-cleanup3
+#git update-ref -d refs/backups/r660429/heads/master
