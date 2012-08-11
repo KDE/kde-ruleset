34ba77bebe591a1dc05cedfdab759d00a8164fe5
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2012-08-11 09:40:05 +0200

    kolf: use common-kdegames rules

---
+++ kdegames/kolf-postprocess.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Execute this script from the converted repository.
+
+git update-ref -d refs/workbranch/kde4_jobflags
+git update-ref -d refs/workbranch/kaction-cleanup1
+git update-ref -d refs/workbranch/kaction-cleanup2
