9c994cd98fbbb5c483db4a285aaa71ed0dc89334
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2012-08-13 00:59:39 +0200

    kreversi: use common-kdegames rules, postprocessing

--- kdegames/kreversi-postprocess.sh
+++ kdegames/kreversi-postprocess.sh
@@ -2,7 +2,6 @@
 
 # Execute this script from the converted repository.
 
-# why are those here at all? rules should have excluded them
 git update-ref -d refs/workbranch/kconfig_new
 git update-ref -d refs/workbranch/kconfiggroup_port
 git update-ref -d refs/workbranch/kde4_jobflags
