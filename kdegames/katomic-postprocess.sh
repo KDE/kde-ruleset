2a99649bc084a71e0d2d5bb622566811bc92701f
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2012-08-12 15:25:28 +0200

    katomic: common-kdegames rules, postprocessing

---
+++ kdegames/katomic-postprocess.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git update-ref -d refs/workbranch/kaction-cleanup1
+git update-ref -d refs/workbranch/kaction-cleanup2
+git update-ref -d refs/workbranch/kde4_kconfig
+git update-ref -d refs/workbranch/kde4_jobflags
+git update-ref -d refs/workbranch/kinstance-redesign
