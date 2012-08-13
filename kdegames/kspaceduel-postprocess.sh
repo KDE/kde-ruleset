3407f38ce5664c7cc9926b63c60710c4ec6caa3b
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2012-08-13 13:58:18 +0200

    kspaceduel: use common-kdegames rules, postprocess

--- kdegames/kspaceduel-postprocess.sh
+++ kdegames/kspaceduel-postprocess.sh
@@ -6,4 +6,3 @@ git update-ref -d refs/workbranch/kde4_jobflags
 git update-ref -d refs/workbranch/kde4_kconfig
 git update-ref -d refs/workbranch/kaction-cleanup1
 git update-ref -d refs/workbranch/kaction-cleanup2
-git update-ref -d refs/workbranch/kaction-cleanup3
