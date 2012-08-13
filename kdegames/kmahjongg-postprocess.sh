5f568e38ba4ff1f5f8ce4874ae6d8c0dc8062aac
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2012-08-13 13:30:34 +0200

    kmahjongg: use common-kdegames rules, postprocess

---
+++ kdegames/kmahjongg-postprocess.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Execute this script from the converted repository.
+
+git update-ref -d refs/workbranch/kde4_jobflags
+git update-ref -d refs/workbranch/kaction-cleanup2
+git update-ref -d refs/workbranch/kconfiggroup_port
