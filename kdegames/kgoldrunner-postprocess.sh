0dc4cfdfbf16f5c33a84d435e321630553c631e2
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2012-08-14 16:55:36 +0200

    kgoldrunner: create branch qgv
    because this head has neither branch nor tag, it would not be pushed

--- kdegames/kgoldrunner-postprocess.sh
+++ kdegames/kgoldrunner-postprocess.sh
@@ -5,3 +5,6 @@
 git update-ref -d refs/workbranch/kde4_jobflags
 git update-ref -d refs/workbranch/kaction-cleanup1
 git update-ref -d refs/workbranch/kaction-cleanup2
+
+# otherwise workbranch/kgoldrunner-qgv will not be pushed
+git branch qgv workbranch/kgoldrunner-qgv
