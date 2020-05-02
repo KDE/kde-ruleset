885a50c4ca93fe412f77a9c2c4ce9ba663e113e7
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2020-05-02 17:32:22 -0300

    Get rid of merge commits in l10n-scripts repo
    A few commits are being marked as merges because they copied a file from
    another branch, but they aren't really merges. I started making a parentmap
    listing SVN revisions fix, but then I realized it affects every merge in
    the repo, so instead I made the postproc script process all merge commits.

--- l10n/scripts-postproc.sh
+++ l10n/scripts-postproc.sh
@@ -4,3 +4,23 @@
 
 # Delete all backup refs and tags
 delete_backup_tags
+
+# A few commits are being marked as merges because they copied a file from
+# another branch, but they aren't really merges. We'll "flatten" the merge,
+# keeping the first parent only.
+# This happens to be the case for *all* merge commits in the converted
+# repository, so we don't need to list each revision to fix, just search all
+# merges and process them all.
+
+git rev-list --merges --parents --all | while read commit parent1 parent2; do
+    echo "clear $commit"
+    echo "$parent1 $commit"
+done > generated-parentmap
+
+git filter-branch \
+    --parent-filter "$(cat generated-parentmap | $RULESETDIR/bin/add-parents.py)" \
+    --tag-name-filter cat \
+    -- --all
+
+rm generated-parentmap
+delete_fb_backups
